3d30a59def3ca7c567318b71abdeb27ba00bff95
Jarbas Santos <jarbells@outlook.com>
2024-05-26 15:54:55 -0300

    Minha versão do exercício

--- src/entities/Order.java
+++ src/entities/Order.java
@@ -1,5 +1,6 @@
 package entities;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -8,6 +9,9 @@ import entities.enums.OrderStatus;
 
 public class Order {
 
+	private static SimpleDateFormat sdf1 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+	private static SimpleDateFormat sdf2 = new SimpleDateFormat("dd/MM/yyyy");
+	
 	private Date moment;
 	private OrderStatus status;
 	
@@ -61,5 +65,37 @@ public class Order {
 			sum += i.subTotal();
 		}
 		return sum;
+	}
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Order moment: ")
+		.append(sdf1.format(moment))
+		.append("\n")
+		.append("Order status: ")
+		.append(status).append("\n")		
+		.append("Client: ")
+		.append(client.getName())
+		.append(" (")
+		.append(sdf2.format(client.getBirthDate()))
+		.append(") - ")
+		.append(client.getEmail())
+		.append("\n")
+		.append("Order items:")
+		.append("\n");
+		double sum = 0.0;
+		for (OrderItem i : items) {
+			sum += i.subTotal();
+			sb.append(i.getProduct().getName())
+			.append(", $")
+			.append(String.format("%.2f", i.getPrice()))
+			.append(", Quantity: ")
+			.append(i.getQuantity())
+			.append(", Subtotal: $")
+			.append(String.format("%.2f%n", i.subTotal()));
+		}
+		sb.append("Total price: $")
+		.append(String.format("%.2f", sum));
+		return sb.toString();
 	}
 }
